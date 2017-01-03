405e29236a660da75cb519a81727069125813b26
Jacob Hackett <jacobrhackett@gmail.com>
2017-01-03 18:26:17 -0500

    refactoring routes

---
+++ app/routes/character.js
@@ -0,0 +1,54 @@
+var User = require('../models/user');
+var Character = require('../models/character');
+
+module.exports = function(app, passport) {
+
+    // =============================================================================
+    // CHARACTER ROUTES ============================================================
+    // =============================================================================
+
+    // CREATE ========================
+    // show the new character form
+    app.get('/character/new', function(req, res) {
+        res.render('character/form.ejs', {
+            user: req.user
+        });
+    });
+
+    // process the new character form
+    app.post('/character/new', function(req, res) {
+        var newCharacter = Character();
+        newCharacter.name = req.body.name;
+        newCharacter.user = req.user;
+        req.user.characters.push(newCharacter);
+        newCharacter.save(function(err) {
+            req.user.save(function(err) {
+                res.redirect('/');
+            });
+        });
+    });
+
+    // READ ========================
+    app.get('/character/:id', function(req, res) {
+        Character.findById(req.params.id, function(err, character) {
+            if (err) {
+                console.log('GET Error: There was a problem retrieving: ' + err);
+            } else {
+                res.render('character/show.ejs', {
+                    user: req.user,
+                    char: character
+                });
+            }
+        });
+    });
+
+    // UPDATE ========================
+    app.put('/character/:id/edit', function(req, res) {
+
+    });
+
+    // DELETE ========================
+    app.delete('/character/:id/edit', function(req, res) {
+
+    });
+};
--- server.js
+++ server.js
@@ -35,7 +35,8 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes/index.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes/character.js')(app, passport);
 
 // launch ======================================================================
 app.listen(port);
