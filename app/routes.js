db6d8d34bbd3fcc7c621e6896bd4342cd3996deb
Jacob Hackett <jacobrhackett@gmail.com>
2017-01-03 13:25:46 -0500

    adding user instance for /

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,9 @@ module.exports = function(app, passport) {
 
     // show the home page (will also have our login links)
     app.get('/', function(req, res) {
-        res.render('index.ejs');
+        res.render('index.ejs', {
+            user : req.user
+        });
     });
 
     // PROFILE SECTION =========================
